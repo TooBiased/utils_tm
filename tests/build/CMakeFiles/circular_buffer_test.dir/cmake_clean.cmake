2a34261a9aaa26070c22acd01aec441b2fd5bde3
tobias.maier2 <tobias.maier2@kit.edu>
2020-06-17 18:08:13 +0200

    Florian

---
+++ tests/build/CMakeFiles/circular_buffer_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circular_buffer_test.dir/src/test_circular_buffer.cpp.o"
+  "circular_buffer_test.pdb"
+  "circular_buffer_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circular_buffer_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/build/CMakeFiles/reclamation_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reclamation_test.dir/src/test_reclamation_strategies.cpp.o"
+  "reclamation_test.pdb"
+  "reclamation_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reclamation_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
